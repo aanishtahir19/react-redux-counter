5e0a35b755718e289e93c47b1d4133491d880e75
fiklehawk <aanishtahir19@gmail.com>
2021-10-23 10:05:22 +0500

    setup initial Template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.scss';
+import logo from './logo.svg'
+function App() {
+  return (
+    <div className="app col-10">
+      <img src={logo} alt="Redux logo" className="app-logo" />
+    </div>
+  )
+}
+
+export default App;
